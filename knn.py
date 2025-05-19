2d0008193b1e27c8980601202f0f1aeeb341af30
Ankush530 <ssanku530@gmail.com>
2025-05-19 15:04:29 +0530

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
 
